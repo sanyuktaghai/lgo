903c82bd8116e1943b6410ba2071604db89935c3
30bySixty <tshestopalova@gmail.com>
2016-08-29 20:42:21 -0500

    style devise_messages

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,5 @@
 class StoriesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_filter :set_story, only: [:show, :edit, :update, :destroy]
   
   def index
