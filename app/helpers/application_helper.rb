35d7a7d7ed660ce5c6ac527fa146e517416fdd2f
30bySixty <tshestopalova@gmail.com>
2016-09-12 11:59:05 -0500

    update user errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,2 @@
 module ApplicationHelper
-  def devise_error_messages
-    return '' if resource.errors.empty?
-    
-    messages = resource.errors.full_messages.map { #this overrides devise error messages
-      |msg| content_tag(:li, msg) }.join
-    html = <<-HTML
-<div class = "callout alert" data-closable>
-  <button class="close-button" aria-label="dismiss alert" type="button" data-close><span aria-hidden="true"> &times;<span></button>
-  <%= content_tag :div, msg if msg.is_a?(String) %>
-</div>
-HTML
-    html.html_safe
-  end
 end
