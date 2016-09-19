79ab6cd23d4091a688393c203e59d396024f57dc
30bySixty <tshestopalova@gmail.com>
2016-09-19 10:31:48 -0500

    add admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919025944) do
+ActiveRecord::Schema.define(version: 20160919152920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,28 +69,20 @@ ActiveRecord::Schema.define(version: 20160919025944) do
     t.index ["user_id"], name: "index_story_likes_on_user_id", using: :btree
   end
 
-  create_table "story_saves", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "story_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["story_id"], name: "index_story_saves_on_story_id", using: :btree
-    t.index ["user_id"], name: "index_story_saves_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
@@ -102,6 +94,4 @@ ActiveRecord::Schema.define(version: 20160919025944) do
   add_foreign_key "stories", "users"
   add_foreign_key "story_likes", "stories"
   add_foreign_key "story_likes", "users"
-  add_foreign_key "story_saves", "stories"
-  add_foreign_key "story_saves", "users"
 end
