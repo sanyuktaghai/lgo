229ad12e36b8832bd937f3360a2b55eeddb011ac
30bySixty <tshestopalova@gmail.com>
2016-09-24 18:34:01 -0500

    refactor deleting_bookmarks_spec per FG user_with_stories

--- spec/features/bookmarks/deleting_bookmarks_spec.rb
+++ spec/features/bookmarks/deleting_bookmarks_spec.rb
@@ -2,11 +2,10 @@ require "rails_helper"
 
 RSpec.feature "Deleting Bookmarks" do
   before do
-    @foo = FactoryGirl.create(:user)
-    @bar = FactoryGirl.create(:user)
-    
-    @story_foo = Story.create(raw_title: "The first story", raw_body: "Body of first story", user: @foo)
-    @story_bar = Story.create(raw_title: "The second story", raw_body: "Body of second story", user: @bar)
+    @bar = FactoryGirl.create(:user_with_stories)
+    @foo = FactoryGirl.create(:user_with_stories)
+    @story_foo = Story.find_by(user_id: @foo.id)
+    @story_bar = Story.find_by(user_id: @bar.id)
     @bookmark = Bookmark.create(user: @foo, story: @story_bar)
   end
   
