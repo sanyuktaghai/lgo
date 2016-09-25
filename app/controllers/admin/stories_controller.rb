24d27173c4aab57def186b02dec2bd17caf2cbe9
30bySixty <tshestopalova@gmail.com>
2016-09-25 12:09:39 -0500

    implement admin show stories controller action and view

--- app/controllers/admin/stories_controller.rb
+++ app/controllers/admin/stories_controller.rb
@@ -1,9 +1,13 @@
 class Admin::StoriesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :require_admin
+  before_action :set_story, only: [:show, :edit, :update, :destroy]
   
   def index
     @stories = Story.unpublished
+  end
+  
+  def show
   end
   
   private
@@ -13,5 +17,9 @@ class Admin::StoriesController < ApplicationController
       redirect_to root_path
       flash[:alert] = "Error."
     end
+  end
+  
+  def set_story
+    @story = Story.find(params[:id])
   end
 end
