c6bc85a805713f586e377948d88fbee07a6146fa
30bySixty <tshestopalova@gmail.com>
2016-09-25 12:24:22 -0500

    update bookmark specs for QC flow

--- spec/features/bookmarks/deleting_bookmarks_spec.rb
+++ spec/features/bookmarks/deleting_bookmarks_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.feature "Deleting Bookmarks" do
   before do
-    @bar = FactoryGirl.create(:user_with_stories)
-    @foo = FactoryGirl.create(:user_with_stories)
+    @bar = FactoryGirl.create(:user_with_published_stories)
+    @foo = FactoryGirl.create(:user_with_published_stories)
     @story_foo = Story.find_by(user_id: @foo.id)
     @story_bar = Story.find_by(user_id: @bar.id)
     @bookmark = Bookmark.create(user: @foo, story: @story_bar)
