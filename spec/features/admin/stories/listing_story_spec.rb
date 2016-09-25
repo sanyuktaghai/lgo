222f99e1ee49859d21f04ef6047dde561dbb727c
30bySixty <tshestopalova@gmail.com>
2016-09-25 12:29:54 -0500

    write admin/listing_story_spec test

---
+++ spec/features/admin/stories/listing_story_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "Listing Stories" do 
+  before do
+    @user = FactoryGirl.create(:user_with_stories, stories_count: 2)
+    @admin = FactoryGirl.create(:admin)
+    @story1 = Story.where(user_id: @user.id).first
+    @story2 = Story.where(user_id: @user.id).last
+    login_as(@admin, :scope => :user)
+  end
+  
+  scenario "Logged-in admin can see list of all stories" do
+    visit "/"
+    
+    expect(page).not_to have_content(@story1.raw_title)
+    expect(page).not_to have_content(@story1.raw_body)
+    expect(page).not_to have_link(@story1.raw_title)
+    
+    visit "/admin"
+    expect(page).to have_content(@story1.raw_title)
+    expect(page).to have_content(@story1.raw_body)
+    expect(page).to have_link(@story1.raw_title)
+    expect(page).to have_content(@story2.raw_title)
+    expect(page).to have_content(@story2.raw_body)
+    expect(page).to have_link(@story2.raw_title)
+  end
+  
+end
