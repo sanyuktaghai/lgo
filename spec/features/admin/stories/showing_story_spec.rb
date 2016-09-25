8a307d0ee48bc0353edf85d814335bf168106d12
30bySixty <tshestopalova@gmail.com>
2016-09-25 12:34:10 -0500

    write admin/showing_story_spec test

---
+++ spec/features/admin/stories/showing_story_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "Showing Stories" do 
+  before do
+    @user = FactoryGirl.create(:user_with_stories)
+    @admin = FactoryGirl.create(:admin)
+    @story = Story.find_by(user_id: @user.id)
+    login_as(@admin, :scope => :user)
+  end
+  
+  scenario "Logged-in admin can see individual story" do
+    visit "/admin"
+    
+    click_link @story.raw_title
+    expect(page).to have_content(@story.raw_title)
+    expect(page).to have_content(@story.raw_body)
+    expect(current_path).to eq(admin_story_path(@story))
+    expect(page).to have_link("Edit Story")
+    expect(page).to have_link("Delete Story")
+  end
+end
