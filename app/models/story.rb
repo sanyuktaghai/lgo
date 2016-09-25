8b10126fde7efe8a9eff8a7397dc7f24751b9269
30bySixty <tshestopalova@gmail.com>
2016-09-25 13:38:38 -0500

    add admin namespace validators to story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,23 +2,17 @@ class Story < ApplicationRecord
   validates :raw_title, presence: true
   validates :raw_body, presence: true
   
+  validates :final_title, presence: true, :if => Proc.new { user.admin? }
+  validates :final_body, presence: true, :if => Proc.new { user.admin? }
+  
   belongs_to :user
-#  belongs_to :admin, class_name: 'User'
-#  belongs_to :poster, class_name: 'User'
-#  belongs_to :author, class_name: 'User'
   
   has_many :comments, dependent: :destroy  # If story gets deleted, the depending comment also gets deleted
   has_many :story_likes, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   
   default_scope { order(created_at: :desc)}
-  
   scope :published, -> { where(published: true) }
   scope :unpublished, -> { where(published: false) }
-  #scope :published { where published: true}
-  # =>  Story.published in controller do the above query
-  # e.g. @ final_stories = Story.published
-  #scope :unpublished {where published: false}
-  # current_user.
   
 end
