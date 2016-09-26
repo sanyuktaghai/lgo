955668b2905d6322dc868a2ef5f14384b30dd613
30bySixty <tshestopalova@gmail.com>
2016-09-26 11:00:34 -0500

    update showing_story_spec.rb for posting anonymous story

--- spec/features/stories/showing_story_spec.rb
+++ spec/features/stories/showing_story_spec.rb
@@ -4,7 +4,8 @@ RSpec.feature "Showing Stories" do
   before do
     @bar = FactoryGirl.create(:user)
     @foo = FactoryGirl.create(:user_with_published_stories)
-    @story = Story.find_by(user_id: @foo.id)
+    @story = Story.find_by(author_id: @foo.id)
+    
   end
   
   scenario "A non-signed in user does not see edit or delete links" do
