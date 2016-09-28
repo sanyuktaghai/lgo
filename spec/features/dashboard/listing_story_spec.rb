89f7103c1e4a1c041215ebc004c6cf46babf44f9
30bySixty <tshestopalova@gmail.com>
2016-09-28 07:43:52 -0500

    write feature spec for /dashboard Listing Stories

---
+++ spec/features/dashboard/listing_story_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "Listing Stories" do 
+  before do
+    @user = FactoryGirl.create(:user_with_unpublished_stories, stories_count: 3)
+    @story1 = Story.where(author_id: @user.id).first
+    @story2 = Story.where(author_id: @user.id).second
+    @story3 = Story.where(author_id: @user.id).last
+    @story3 = @story3.update(published: true, final_title: "Title of the published story", final_body: "Body of the final story")
+  end
+  
+  scenario "Logged-in user can see list of all published and unpublished stories" do
+    visit "/dashboard"
+    
+    expect(page).to have_content(@story1.raw_title)
+    expect(page).to have_content(@story1.raw_body)
+    expect(page).to have_link(@story1.raw_title)
+    expect(page).to have_content(@story2.raw_title)
+    expect(page).to have_content(@story2.raw_body)
+    expect(page).to have_link(@story2.raw_title)
+    expect(page).to have_content(@story3.final_title)
+    expect(page).to have_content(@story3.final_body)
+    expect(page).to have_link(@story3.final_title)
+  end
+end
