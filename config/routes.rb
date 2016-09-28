c3a87d753b13b6631528e333e77949b75041893c
30bySixty <tshestopalova@gmail.com>
2016-09-28 10:23:24 -0500

    replace root route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 #root 'welcome#index'
   
+  root to: 'stories#index'
+  
   resources :stories do 
     resources :comments
     resources :story_likes
