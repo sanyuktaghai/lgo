5610fa260ba99d62aba1f171d5eb26b3617f9466
30bySixty <tshestopalova@gmail.com>
2016-09-29 10:34:14 -0500

    update dashboard for showing 'anonymous' on listing stories partial view

--- spec/features/dashboard/listing_authored_stories_spec.rb
+++ spec/features/dashboard/listing_authored_stories_spec.rb
@@ -2,17 +2,19 @@ require 'rails_helper'
 
 RSpec.feature "Listing Stories" do 
   before do
-    @user = FactoryGirl.create(:user_with_unpublished_stories, stories_count: 3)
+    @user = FactoryGirl.create(:user_with_unpublished_stories, stories_count: 4)
     @story1 = Story.where(author_id: @user.id).first
     @story2 = Story.where(author_id: @user.id).second
-    @story3 = Story.where(author_id: @user.id).last
+    @story3 = Story.where(author_id: @user.id).third
+    @story4 = Story.where(author_id: @user.id).last
     @story3.update(published: true, final_title: Faker::Hipster.sentence, final_body: Faker::Hipster.paragraph)
+    @story4.update(published: true, final_title: Faker::Hipster.sentence, final_body: Faker::Hipster.paragraph, anonymous: true)
     login_as(@user, :scope => :user)
   end
   
-  scenario "Logged-in user can see full list of her published and unpublished stories" do
+  scenario "Logged-in user can see full list of her published, unpublished, and anonymous stories" do
     visit(dashboard_path(@user))
-    expect(page).to have_content("Stories: 3")
+    expect(page).to have_content("Stories: 4")
     
     expect(page).to have_content(Story.where(author_id: @user.id).count)
     expect(page).to have_content(@story1.raw_title)
@@ -24,6 +26,12 @@ RSpec.feature "Listing Stories" do
     expect(page).to have_content(@story3.final_title)
     expect(page).to have_content(@story3.final_body.truncate(150))
     expect(page).to have_link(@story3.final_title)
+    expect(page).to have_content("Pending")
+    
+    expect(page).to have_content(@story4.final_title)
+    expect(page).to have_content(@story4.final_body.truncate(150))
+    expect(page).to have_link(@story4.final_title)
+    expect(page).to have_content("Anonymous")
    end
   
   scenario "Logged-in user can click back to her full list of published and unpublished stories", js: true do
