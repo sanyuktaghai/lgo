971c436964de5f87e1efd500e7f0b41389f921be
30bySixty <tshestopalova@gmail.com>
2016-09-28 23:21:59 -0500

    implement controller show and liked_stories actions

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,22 @@
 class DashboardController < ApplicationController
+  before_action :set_user, only: [:show, :liked_stories]
+  
   def show
-    @user = User.find(params[:id])
-    @stories = Story.where(author_id: @user)
+    @stories = Story.all
+    @authored_stories = @stories.where(author_id: @user)
   end
+  
+  def liked_stories
+    @liked_stories = Story.all.where.not(author_id: @user.id).joins(:story_likes).where(:story_likes => { :user_id => @user.id})
+    respond_to do |format|
+      format.js {render :partial => 'dashboard/liked_stories'} 
+    end
+  end
+  
+  private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
 end
