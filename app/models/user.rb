b79f74da9d48570e8bca888884ce910e0c82346a
30bySixty <tshestopalova@gmail.com>
2016-10-01 22:03:25 -0500

    add 'Follow' link to dashboard and update User model for link method

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,19 @@ class User < ApplicationRecord
   has_many :stories  
   has_many :story_likes
   has_many :bookmarks
+  has_many :followings
+  has_many :followers, through: :followings, class_name: "User"
   
   validates :first_name, presence: true
   validates :last_name, presence: true
   
   def full_name
-    "#{self.first_name.titleize.gsub(/\b\w/) { |w| w.upcase }} #{self.last_name.titleize.gsub(/\b\w/) { |w| w.upcase }}"
+    f_name = self.first_name.titleize.gsub(/\b\w/) { |w| w.upcase }
+    l_name = self.last_name.titleize.gsub(/\b\w/) { |w| w.upcase }
+    "#{f_name} #{l_name}"
+  end
+
+  def follows_or_same?(new_follow)
+    followings.map(&:follower).include?(new_follow) || self == new_follow
   end
 end
