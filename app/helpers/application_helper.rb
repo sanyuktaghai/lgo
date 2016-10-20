2ea1f584fb0701393026716c461c5634b8a48d27
30bySixty <tshestopalova@gmail.com>
2016-10-20 00:54:58 -0500

    remove 'signed-in-as-email' message from header for multi-step form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def signed_in_user
+    unless current_page? registration_step_path(:basic_details)
+      html = <<-HTML
+<p class="navbar-text">Signed in as #{current_user.email}.</p>
+HTML
+      html.html_safe
+    end
+  end
 end
