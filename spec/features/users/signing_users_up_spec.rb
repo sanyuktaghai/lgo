ff71d57c1223c308c50cb0aed5f75d76f4453d31
30bySixty <tshestopalova@gmail.com>
2016-10-20 01:01:55 -0500

    update sign-users-up spec for invalid credentials scenario

--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -5,7 +5,9 @@ RSpec.feature "Users signup" do
   before do
     @email = Faker::Internet.email
     @password = Faker::Internet.password
+    @password2 = Faker::Internet.password
   end
+  
   scenario "with valid credentials" do
     visit "/"
     
@@ -26,5 +28,37 @@ RSpec.feature "Users signup" do
     
     expect(page).to have_content("You have signed up successfully.")
     expect(page).to have_content("Signed in as #{@email}")
+  end
+  
+  scenario "with invalid credentials - step 1" do
+    visit "/"
+    
+    click_link "Sign up"
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    
+    click_button "Sign up"
+    
+    expect(page).to have_content("Email can't be blank")
+    expect(page).to have_content("Password can't be blank")
+  end
+  
+  scenario "with invalid credentials - step 1" do
+    visit "/"
+    
+    click_link "Sign up"
+    fill_in "Email", with: Faker::Internet.email
+    fill_in "Password", with: @password2
+    fill_in "Password confirmation", with: @password2
+    
+    click_button "Sign up"
+    
+    fill_in "First Name", with: ""
+    fill_in "Last Name", with: ""
+    
+    click_button "Submit"
+    
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
   end
 end
