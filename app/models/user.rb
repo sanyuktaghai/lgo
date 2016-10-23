a18575c01e492b30b6a77f26e826c23dc365fb69
30bySixty <tshestopalova@gmail.com>
2016-10-23 15:24:00 -0500

    add paperclip methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,4 +80,12 @@ class User < ApplicationRecord
 #    "http://graph.facebook.com/#{self.uid}/picture?type=square&type=large"
     "http://graph.facebook.com/#{self.uid}/picture?type=square&width=200&height=200"
   end
+  
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+    }
+  
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
