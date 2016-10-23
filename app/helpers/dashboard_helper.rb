0421ae071b92e784a5751e90e4a0fac9fd2c0d70
30bySixty <tshestopalova@gmail.com>
2016-10-23 18:56:30 -0500

    implement show profile picture on user dashboard

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,9 +1,9 @@
 module DashboardHelper
   def user_photo(user)
-    if @user.fbimage?
-      image_tag(@user.largesquareimage)
-    elsif @user.image_file_name?
+    if @user.image_file_name?
       image_tag @user.image.url(:medium)
+    elsif @user.fbimage?
+      image_tag(@user.largesquareimage)
     else
       link_to "Upload Photo", edit_user_dashboard_registration_path(@user), remote: true
     end
