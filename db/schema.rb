f276e84e0cec5d05c554567ea536b0cd46224bde
30bySixty <tshestopalova@gmail.com>
2016-10-23 15:50:24 -0500

    migrate paperclip image classes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021210527) do
+ActiveRecord::Schema.define(version: 20161023202303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,12 +44,8 @@ ActiveRecord::Schema.define(version: 20161021210527) do
   end
 
   create_table "identities", force: :cascade do |t|
-    t.string   "uid"
-    t.string   "provider"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_identities_on_user_id", using: :btree
   end
 
   create_table "models", force: :cascade do |t|
@@ -123,6 +119,10 @@ ActiveRecord::Schema.define(version: 20161021210527) do
     t.string   "age_range"
     t.string   "image"
     t.date     "birthday"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
