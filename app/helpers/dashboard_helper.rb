57481f3cb84b6bcfeb81f1b23babbc7517fb1f24
30bySixty <tshestopalova@gmail.com>
2016-10-23 16:01:04 -0500

    implement form to upload user image

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,6 +2,8 @@ module DashboardHelper
   def user_photo(user)
     if @user.image?
       image_tag(@user.largesquareimage)
+    else
+      link_to "Upload Photo", edit_user_dashboard_registration_path(@user), remote: true
     end
   end
 end
