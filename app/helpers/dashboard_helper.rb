badf08e31399798633b18fb6851b1fb168336bad
30bySixty <tshestopalova@gmail.com>
2016-10-24 19:30:09 -0500

    fix bug so that users can only upload photos on their dashboards

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,11 +1,13 @@
 module DashboardHelper
   def user_photo(user)
-    if @user.image_file_name?
-      image_tag @user.image.url(:medium)
-    elsif @user.fbimage?
-      image_tag(@user.largesquareimage)
-    else
-      link_to "Upload Photo", edit_user_dashboard_registration_path(@user), remote: true
+    if @user == current_user
+      if @user.image_file_name?
+        image_tag @user.image.url(:medium)
+      elsif @user.fbimage?
+        image_tag(@user.largesquareimage)
+      else
+        link_to "Upload Photo", edit_user_dashboard_registration_path(@user), remote: true
+        end
     end
   end
 end
