d176bd54b5749165b37e91f54f0f80d2da415d93
30bySixty <tshestopalova@gmail.com>
2016-11-06 12:01:59 -0600

    update tests for all 5 possible reactions

--- spec/features/reactions/creating_reaction_lol_spec.rb
+++ spec/features/reactions/creating_reaction_lol_spec.rb
@@ -5,7 +5,13 @@ RSpec.feature "Adding Reaction_Lol to Stories" do
     @bar = FactoryGirl.create(:user)
     @foo = FactoryGirl.create(:user_with_published_stories)
     @story = Story.find_by(author_id: @foo.id)
-    ReactionCategory.create(id: 3, name: 'lol')
+    ReactionCategory.create([
+      {id: 1, name: 'like'}, 
+      {id: 2, name: 'omg'}, 
+      {id: 3, name: 'lol'}, 
+      {id: 4, name: 'cool'}, 
+      {id: 5, name: 'love'}
+      ])
   end
   
   scenario "Permit a signed in user to lol a story", :js => true do
@@ -14,9 +20,17 @@ RSpec.feature "Adding Reaction_Lol to Stories" do
     visit "/"
     click_link @story.final_title
   
-    click_link "LOL Story"
+#    click_on "Like Story"
+    click_on "OMG Story"
+    click_on "LOL Story"
+    click_on "Cool Story"
+    click_on "Love Story"
     
+#    expect(page).to have_content("Likes: 1")
+    expect(page).to have_content("OMGs: 1")
     expect(page).to have_content("LOLs: 1")
+    expect(page).to have_content("Cools: 1")
+    expect(page).to have_content("Loves: 1")
     expect(page.current_path).to eq(story_path(@story))
   end
   
@@ -24,7 +38,7 @@ RSpec.feature "Adding Reaction_Lol to Stories" do
     visit "/"
     click_link @story.final_title
     
-    click_link "LOL Story"
+    click_on "LOL Story"
     
     expect(page).to have_content("You need to sign in or sign up before continuing.")
     expect(page.current_path).to eq(new_user_session_path)
--- spec/features/reactions/deleting_reaction_lol_spec.rb
+++ spec/features/reactions/deleting_reaction_lol_spec.rb
@@ -6,20 +6,48 @@ RSpec.feature "Deleting Reaction_Lol" do
     @foo = FactoryGirl.create(:user_with_published_stories)
     @story_foo = Story.find_by(author_id: @foo.id)
     @story_bar = Story.find_by(author_id: @bar.id)
-    ReactionCategory.create(id: 3, name: 'lol')
-    @story_lol = Reaction.create(user: @foo, story: @story_bar, reaction_category_id: 3)
+    ReactionCategory.create([
+      {id: 1, name: 'like'}, 
+      {id: 2, name: 'omg'}, 
+      {id: 3, name: 'lol'}, 
+      {id: 4, name: 'cool'}, 
+      {id: 5, name: 'love'}
+    ])
+    Reaction.create([
+      {user: @foo, story: @story_bar, reaction_category_id: 1},
+      {user: @foo, story: @story_bar, reaction_category_id: 2},
+      {user: @foo, story: @story_bar, reaction_category_id: 3},
+      {user: @foo, story: @story_bar, reaction_category_id: 4},
+      {user: @foo, story: @story_bar, reaction_category_id: 5},
+    ])
   end
   
   scenario "A user succeeds", :js => true do
     login_as(@foo, :scope => :user)
     
     visit "/"
-    click_link @story_bar.final_title  
-    click_link "UnLOL Story"
+    click_link @story_bar.final_title 
+    click_on "UnLike"
+    click_on "UnOMG"
+    click_on "UnLOL"
+    click_on "UnCool"
+    click_on "UnLove"
     
-    expect(page).to have_content("LOL has been deleted")
+    expect(page).to have_content("Likes: 0")
+    expect(page).to have_content("OMGs: 0")
     expect(page).to have_content("LOLs: 0")
-    expect(page).to have_button("LOL Story")
+    expect(page).to have_content("Cools: 0")
+    expect(page).to have_content("Loves: 0")
+    
+    expect(page).to have_link("Like Story")
+    expect(page).to have_link("OMG Story")
+    expect(page).to have_link("LOL Story")
+    expect(page).to have_link("Cool Story")
+    expect(page).to have_link("Love Story")
+    expect(page).not_to have_link("UnLike Story")
+    expect(page).not_to have_link("UnOMG Story")
     expect(page).not_to have_link("UnLOL Story")
+    expect(page).not_to have_link("UnCool Story")
+    expect(page).not_to have_link("UnLove Story")
   end
 end
