2a33c98dc5591c0b0044f5260079ed9822d84ec5
30bySixty <tshestopalova@gmail.com>
2016-11-05 19:10:55 -0500

    create Reaction_Category model

---
+++ app/models/reaction_category.rb
@@ -0,0 +1,2 @@
+class ReactionCategory < ApplicationRecord
+end
---
+++ db/migrate/20161106000829_create_reaction_categories.rb
@@ -0,0 +1,8 @@
+class CreateReactionCategories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :reaction_categories do |t|
+      
+      t.timestamps
+    end
+  end
+end
