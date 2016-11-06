80bcc11e8b958db72d77feebe3594ed5b427be19
30bySixty <tshestopalova@gmail.com>
2016-11-06 11:31:37 -0600

    update reactions controller for remote:true

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -2,38 +2,30 @@ class ReactionsController < ApplicationController
   before_action :authenticate_user!
   
   def create
-#    unless current_user
-#      flash[:warning] = "Please sign in to continue"
-#      redirect_to new_user_session_path
-#    else 
     @reaction = Reaction.create(reaction_params)
-#    @select_text = @reaction.select_text
     if @reaction.save
       respond_to do |format|
-        flash[:success] = "You Lol'd the story"
-        format.js {render :data => @reaction.to_json }
-#        format.js {render "create", :locals => {:select_text => @select_text}}
-#        format.html { redirect_to story_path(Story.find(@reaction.story_id)) }
+        @story = Story.find(@reaction.story_id)
+        flash.now[:success] = "You reacted to the story"
+        format.js {render :data => [ @reaction.to_json, @story.to_json] }
       end
     else
       flash[:warning] = "Your reaction could not be recorded."
       redirect_to story_path(Story.find(@reaction.story_id))
-#      end
     end
   end
   
   def destroy
-    @reaction = Reaction.where(reaction_params).first
-    @story_id = @reaction.story_id
-#    binding.pry
-    if @reaction.destroy
-      respond_to do |format|
-        flash.now[:success] = "You UnLol'd the story"
-        format.js
+    @reaction = Reaction.find(params[:id])
+    @story = Story.find(@reaction.story_id)
+    respond_to do |format|
+      if @reaction.destroy
+        flash.now[:success] = "You un-reacted to the story"
+        format.js {render :data => @reaction.to_json }
+      else
+        flash[:danger] = "Your reaction could not be removed."
+        redirect_to story_path(Story.find(@reaction.story_id))
       end
-    else
-      flash[:danger] = "Your reaction could not be removed."
-      redirect_to story_path(Story.find(@reaction.story_id))
     end
   end
   
