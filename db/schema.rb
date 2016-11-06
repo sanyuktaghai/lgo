d3e515411a249b68df175d7b2ca7fbf6c668c640
30bySixty <tshestopalova@gmail.com>
2016-11-06 12:11:49 -0600

    drop story_likes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106001718) do
+ActiveRecord::Schema.define(version: 20161106181001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,15 +121,6 @@ ActiveRecord::Schema.define(version: 20161106001718) do
     t.index ["user_id"], name: "index_stories_on_user_id", using: :btree
   end
 
-  create_table "story_likes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "story_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["story_id"], name: "index_story_likes_on_story_id", using: :btree
-    t.index ["user_id"], name: "index_story_likes_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
@@ -176,6 +167,4 @@ ActiveRecord::Schema.define(version: 20161106001718) do
   add_foreign_key "stories", "users", column: "admin_id"
   add_foreign_key "stories", "users", column: "author_id"
   add_foreign_key "stories", "users", column: "poster_id"
-  add_foreign_key "story_likes", "stories"
-  add_foreign_key "story_likes", "users"
 end
