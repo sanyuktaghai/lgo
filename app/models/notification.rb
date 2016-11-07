599245ef43dd534c682fb3606ffef85cd883cc77
30bySixty <tshestopalova@gmail.com>
2016-11-06 20:51:14 -0600

    add unread scope to notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,6 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :notified_by_user, class_name: "User"
   belongs_to :notification_category
+  
+  scope :unread, -> { where(read: false)}
 end
