31054259dbc13bc5a717039f18960bebd8c64324
30bySixty <tshestopalova@gmail.com>
2016-11-06 21:52:41 -0600

    implement feature for users to be notified of new followings

--- spec/features/dashboard/notifications/listing_notifications_spec.rb
+++ spec/features/dashboard/notifications/listing_notifications_spec.rb
@@ -4,8 +4,14 @@ RSpec.feature "Listing Notifications" do
   before do
     @foo = FactoryGirl.create(:user)
     @bar = FactoryGirl.create(:user)
-    NotificationCategory.create(id: 1, name: "following")
-    Notification.create(user: @foo, notified_by_user: @bar, notification_category_id: 1, read: false)
+    NotificationCategory.create([
+      {id: 1, name: "Story"},
+      {id: 2, name: "Comment"},
+      {id: 3, name: "Reaction"},
+      {id: 4, name: "Bookmark"},
+      {id: 5, name: "Following"}
+      ])
+    Notification.create(user: @foo, notified_by_user: @bar, notification_category_id: 5, read: false)
     login_as(@foo, :scope => :user)
   end
   
@@ -13,6 +19,7 @@ RSpec.feature "Listing Notifications" do
     visit(dashboard_path(@foo))
     click_link "Notifications"
     
-    expect(page).to have_content("#{@foo.full_name} followed you.")
+    expect(page).to have_content("#{@bar.full_name} followed you.")
+    expect(page).to have_link("#{@bar.full_name}")
   end
 end
