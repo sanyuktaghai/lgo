dfb0bd126c07bd1fa2c70ee3a97788ca854973b9
30bySixty <tshestopalova@gmail.com>
2016-11-06 22:09:28 -0600

    update notification test for story-published notifications:

--- spec/features/dashboard/notifications/listing_notifications_spec.rb
+++ spec/features/dashboard/notifications/listing_notifications_spec.rb
@@ -11,7 +11,12 @@ RSpec.feature "Listing Notifications" do
       {id: 4, name: "Bookmark"},
       {id: 5, name: "Following"}
       ])
+    @story = Story.create(
+      final_title: Faker::Hipster.sentence,
+      published: true,
+      poster_id: @foo.id)
     Notification.create(user: @foo, notified_by_user: @bar, notification_category_id: 5, read: false)
+    Notification.create(user: @foo, notification_category_id: 1, read: false, origin_id: @story.id)
     login_as(@foo, :scope => :user)
   end
   
@@ -19,6 +24,11 @@ RSpec.feature "Listing Notifications" do
     visit(dashboard_path(@foo))
     click_link "Notifications"
     
+    #published story notifications
+    expect(page).to have_content("Your story has been published: ")
+    expect(page).to have_content(@story.final_title)
+    
+    #followings notifications
     expect(page).to have_content("#{@bar.full_name} followed you.")
     expect(page).to have_link("#{@bar.full_name}")
   end
