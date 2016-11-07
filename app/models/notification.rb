5181006c34cc1ab9055e7ea4c412c307cde80c34
30bySixty <tshestopalova@gmail.com>
2016-11-06 20:47:41 -0600

    fix typo in Notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,5 @@
 class Notification < ApplicationRecord
   belongs_to :user
-  belongs_to :notified_by_user_id, class_name: "User"
+  belongs_to :notified_by_user, class_name: "User"
   belongs_to :notification_category
 end
